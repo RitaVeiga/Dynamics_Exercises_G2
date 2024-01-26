3f6070ac1f637d7e4a965cede9a53b5ba52b01e9
tiagofvfalcao <tiagof2003@gmail.com>
2024-01-26 11:33:22 +0000

    update Account-g2

---
+++ Dynamics_Exercises_G2/Scripts/Account-g2.js
@@ -0,0 +1,36 @@
+﻿var AccountHelper = {
+
+    OnLoad: {
+        OnMainFormLoads: function (executionContext) {
+            try {
+                debugger;
+                GeneralHelperFunctions.SetContentToWebResource(executionContext);
+            } catch (e) {
+                console.log(e.message);
+            }
+        },
+
+    },
+
+    OnClick: {
+
+    }
+}
+
+var GeneralHelperFunctions = {
+    SetContentToWebResource: function (executionContext) {
+        try {
+            var formContext = executionContext.getFormContext();
+            var wrControl = formContext.getControl("WebResource_IncidentList_G2");
+            if (wrControl !== null) {
+                wrControl.getContentWindow().then(
+                    function (contentWindow) {
+                        contentWindow.HelperFunctions.SetClientApiContext(Xrm, formContext);
+                    }
+                )
+            }
+        } catch (e) {
+            console.log(e.message);
+        }
+    }
+}
